f96c1051adc2c90a85a8b932c2eca282b2de92d3
krishnathumma <krishna.thumma9@gmail.com>
2024-09-19 13:00:59 -0400

    Student Management System with SQLlite

---
+++ studentMS/example.py
@@ -0,0 +1,48 @@
+import sys
+from datetime import datetime
+
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QLineEdit, \
+    QPushButton
+
+
+class AgeCalculator(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Age Calculator")
+        grid = QGridLayout()
+
+        # create Widgets
+        name_label = QLabel("Name : ")
+        self.name_line_edit = QLineEdit()
+
+        dob_label = QLabel("Date of Birth MM/DD/YYYY : ")
+        self.dob_line_edit = QLineEdit()
+
+        calculate_button = QPushButton("Calculate Age")
+        calculate_button.clicked.connect(self.calculate_age)
+        self.output_label = QLabel("")
+
+        # Add Widgets to Grid
+        grid.addWidget(name_label, 0, 0)
+        grid.addWidget(self.name_line_edit, 0, 1)
+
+        grid.addWidget(dob_label, 1, 0)
+        grid.addWidget(self.dob_line_edit, 1, 1)
+
+        grid.addWidget(calculate_button, 2, 0, 1, 2)
+        grid.addWidget(self.output_label, 3, 0, 1, 2)
+
+        self.setLayout(grid)
+
+    def calculate_age(self):
+        current_year = datetime.now().year
+        dob = self.dob_line_edit.text()
+        dob_year = datetime.strptime(dob, "%m/%d/%Y").date().year
+        age = current_year - dob_year
+        self.output_label.setText(f"{self.name_line_edit.text()} is {age} years Old")
+
+
+app = QApplication(sys.argv)
+age_calculator = AgeCalculator()
+age_calculator.show()
+sys.exit(app.exec())
